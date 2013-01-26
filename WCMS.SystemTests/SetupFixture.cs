e78af047218bb9bf8b4cb8d0d95793b72dee387b
Tom Walker <tpwalke2@gmail.com>
2013-01-26 09:19:40 -0500

    Add UI tests

---
+++ WCMS.SystemTests/SetupFixture.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using NUnit.Framework;
+using WCMS.UITestEngine;
+
+namespace WCMS.SystemTests
+{
+  [SetUpFixture]
+  class SetupFixture : BaseSetupFixture
+  {
+    [SetUp]
+    public void Setup()
+    {
+      DefaultSetUp(ConfigurationManager.AppSettings["TestWebBrowser"]);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      DefaultTearDown();
+    }
+  }
+}
